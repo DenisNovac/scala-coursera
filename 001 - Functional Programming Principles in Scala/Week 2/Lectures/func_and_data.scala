c9835c697239fee4c84c3b65a0f53d68b4d9bafc
DenisNovac <ponydnk@gmail.com>
2019-12-08 15:32:56 +0500

    Модель замен в классах, типы операторов, инфиксная нотация, старшинство операторов

--- 001 - Functional Programming Principles in Scala/Week 2/Lectures/func_and_data.scala
+++ 001 - Functional Programming Principles in Scala/Week 2/Lectures/func_and_data.scala
@@ -1,3 +1,5 @@
+
+
 object Main extends App {
   val r = new Rational(1,2)
   // число - это хеш объекта
@@ -6,19 +8,19 @@ object Main extends App {
   println(r.numer) // 1
 
   val rr = new Rational(2,3)
-  val n = r.add(rr)
+  val n = r + rr
   println(n) // 7/6
 
   val x = new Rational(1,3)
   val y = new Rational(5,7)
   val z = new Rational(3,2)
 
-  println(x.sub(y).sub(z)) // -79/42
+  println(x - y - z) // -79/42
 
-  println(y.add(y)) // 70/49  -- 10/7
+  println(y + y) // 70/49  -- 10/7
 
-  println(y.less(x))
-  println(y.max(x))
+  println(y < x)
+  println(y max x)
 
   println(new Rational(42)) // 42/1
 
@@ -34,19 +36,20 @@ class Rational(x: Int, y: Int) {
   def this(x: Int) = this(x, 1)
 
 
-  def add(that: Rational): Rational = 
+  def + (that: Rational): Rational = 
     new Rational(
       numer * that.denom + that.numer * denom,
       denom * that.denom)
 
-  def neg: Rational = new Rational(-numer, denom)
+  // пробел между - и : обязателен!
+  def unary_- : Rational = new Rational(-numer, denom)
 
   // для вычитания прибавляем отрицательное число, меньше повтора кода
-  def sub(that: Rational): Rational = add(that.neg)
+  def - (that: Rational): Rational = this + -that
 
-  def less(that: Rational) = numer * that.denom < that.numer * denom
+  def < (that: Rational) = numer * that.denom < that.numer * denom
 
-  def max(that: Rational) = if (this.less(that)) that else this
+  def max(that: Rational) = if (this < that) that else this
 
 
   private def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
